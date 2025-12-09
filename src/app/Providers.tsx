abffcd542eaaa348e9aeeeaf22a6a249120d8ee6
Julie Lenhof <julielenhof@MacBook-Air-de-Julie.local>
2025-12-09 21:05:49 +0100

    Simplify providers and profil/signup pages to fix build

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
-import { SupabaseProvider } from "../providers/supabase-provider";
-import ThemeInit from "../components/ThemeInit";
 
 export default function AppProviders({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <ThemeInit />
-      <SessionProvider>
-        <SupabaseProvider>{children}</SupabaseProvider>
-      </SessionProvider>
-    </>
-  );
+  // Version simplifiée temporaire : pas de SessionProvider, pas de SupabaseProvider
+  return <>{children}</>;
 }
